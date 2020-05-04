e43fe0e2278b7ccf04ecbf72b5ded90f715293b3
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-05-04 20:21:16 +0200

    minor fix

--- Install Better Discord.sh
+++ Install Better Discord.sh
@@ -1,15 +1,26 @@
-echo "Duplicating Discord..."
-cp -r /Applications/Discord.app /Applications/Better\ Discord.app
-cd /Applications/Better\ Discord.app/Contents/Resources/
-if [[ -d "./app/" ]]
+cd /Applications
+if [[ -d "Better Discord.app" ]]
 then
-	echo "Attempting to delete old app folder, please check the folder before deleting!"
-	rm -r -v /Applications/Better\ Discord.app/Contents/Resources/app
+	echo "You have already installed it! Remove it to reinstall"
 fi
-echo "Downloading..."
-curl -L -o injector.zip https://github.com/rauenzi/BetterDiscordApp/archive/injector.zip
-echo "Renaming..."
-mv ./BetterDiscordApp-injector/ ./app/
-echo "Done!"
-echo "Opening application..."
-open -a "/Applications/Better Discord.app"
+if [[ ! -d "Better Discord.app" ]]
+then
+	echo "Duplicating Discord..."
+	cp -r /Applications/Discord.app /Applications/Better\ Discord.app
+	cd /Applications/Better\ Discord.app/Contents/Resources/
+	if [[ -d "./app/" ]]
+	then
+		echo "Attempting to delete old app folder, please check the folder before deleting!"
+		rm -r -v /Applications/Better\ Discord.app/Contents/Resources/app
+	fi
+	echo "Downloading..."
+	curl -L -o injector.zip https://github.com/rauenzi/BetterDiscordApp/archive/injector.zip
+	unzip injector.zip
+	echo "Renaming..."
+	mv ./BetterDiscordApp-injector/ ./app/
+	echo "Cleaning up..."
+	rm ./injector.zip
+	echo "Opening application..."
+	open -a "/Applications/Better Discord.app"
+	echo "Well, guess i'm finished"
+fi
