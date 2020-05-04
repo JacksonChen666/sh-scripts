e78e8b63bdbe62a6ba25af0d4bb6e67fcef66ec0
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-05-04 21:54:28 +0200

    Colors

--- Install Better Discord.sh
+++ Install Better Discord.sh
@@ -1,30 +1,36 @@
+#!/bin/bash
 cd /Applications
+RED='\033[91m'
+VERYRED='\033[31m'
+BLUE='\033[34m'
+PROGRESS='\033[38;5;247m'
+GREEN='\033[92m'
+RESET='\033[0m'
 if [[ -d "Better Discord.app" ]]
 then
-	echo "You have already installed it! Remove it to reinstall"
+	echo -e "${RED}You have already installed it! Remove it to reinstall${RESET}"
 fi
 if [[ ! -d "Discord.app" ]]
 then
-	echo "Discord not found. Please make sure it is in the applications folder."
+	echo -e "${RED}Discord not found. Please make sure it is in the applications folder${RESET}"
 fi
 if [[ ! -d "Better Discord.app" ]] && [[ -d "Discord.app" ]]
 then
-	echo "Duplicating Discord..."
+	echo -e "Duplicating Discord...${RESET}"
 	cp -r /Applications/Discord.app /Applications/Better\ Discord.app
 	cd /Applications/Better\ Discord.app/Contents/Resources/
 	if [[ -d "./app/" ]]
 	then
-		echo "Attempting to delete old app folder, please check the folder before deleting!"
+		echo -e "${VERYRED}Attempting to delete old app folder, please check the folder before deleting!${RESET}"
 		rm -r -v /Applications/Better\ Discord.app/Contents/Resources/app
 	fi
-	echo "Downloading..."
+	echo -e "${RESET}Downloading...${PROGRESS}"
 	curl -L -o injector.zip https://github.com/rauenzi/BetterDiscordApp/archive/injector.zip
+	echo -e "${RESET}Extracting...${PROGRESS}"
 	unzip injector.zip
-	echo "Renaming..."
+	echo -e "${RESET}Renaming folder..."
 	mv ./BetterDiscordApp-injector/ ./app/
-	echo "Cleaning up..."
+	echo -e "Cleaning up..."
 	rm ./injector.zip
-	echo "Opening application..."
-	open -a "/Applications/Better Discord.app"
-	echo "Well, guess i'm finished"
+	echo -e "${GREEN}Well, guess i'm finished${RESET}"
 fi
