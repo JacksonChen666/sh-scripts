eb301a36bcc31f75c5f7061faf6966e3dda8e213
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-05-06 15:44:04 +0200

    improved & prevents the wrong system

--- Install Better Discord.sh
+++ Install Better Discord.sh
@@ -1,36 +1,60 @@
 #!/bin/bash
-cd /Applications
 RED='\033[91m'
 VERYRED='\033[31m'
 BLUE='\033[34m'
 PROGRESS='\033[38;5;247m'
 GREEN='\033[92m'
 RESET='\033[0m'
-if [[ -d "Better Discord.app" ]]
+FILENAME='injector.zip'
+OS=$(uname)
+if [[ ! $OS == "Darwin" ]]
 then
-	echo -e "${RED}You have already installed it! Remove it to reinstall${RESET}"
+	echo -e "${RED}Unsupported system to install on. Manually install it or whatever.${RESET}";
+	exit 0;
 fi
-if [[ ! -d "Discord.app" ]]
-then
-	echo -e "${RED}Discord not found. Please make sure it is in the applications folder${RESET}"
-fi
-if [[ ! -d "Better Discord.app" ]] && [[ -d "Discord.app" ]]
-then
-	echo -e "Duplicating Discord...${RESET}"
+cd /Applications
+function installBD () 
+{
+	[[ ! -d "Discord.app" ]] && echo -e "${RED}Discord not found. Please make sure it is in the applications folder.${RESET}"; exit;
+	[[ -d "Better Discord.app" ]] && rm -r /Applications/Better\ Discord.app
+	echo -e "${PROGRESS}Downloading while doing other stuff${RESET}" &
+	curl -L -o $FILENAME https://github.com/rauenzi/BetterDiscordApp/archive/injector.zip &
+	echo -e "Duplicating Discord...${RESET}" &
 	cp -r /Applications/Discord.app /Applications/Better\ Discord.app
 	cd /Applications/Better\ Discord.app/Contents/Resources/
 	if [[ -d "./app/" ]]
 	then
-		echo -e "${VERYRED}Attempting to delete old app folder, please check the folder before deleting!${RESET}"
+		echo -e "${VERYRED}Attempting to delete old app folder, please check the folder before deleting!${RESET}" &
 		rm -r -v /Applications/Better\ Discord.app/Contents/Resources/app
 	fi
-	echo -e "${RESET}Downloading...${PROGRESS}"
-	curl -L -o injector.zip https://github.com/rauenzi/BetterDiscordApp/archive/injector.zip
-	echo -e "${RESET}Extracting...${PROGRESS}"
-	unzip injector.zip
-	echo -e "${RESET}Renaming folder..."
+	while [ ! -f "/Applications/$FILENAME" ]; do sleep 0.1; done
+	echo -e "${RESET}Installing...${PROGRESS}"
+	mv /Applications/$FILENAME ./
+	unzip $FILENAME
 	mv ./BetterDiscordApp-injector/ ./app/
 	echo -e "Cleaning up..."
-	rm ./injector.zip
-	echo -e "${GREEN}Well, guess i'm finished${RESET}"
+	rm ./$FILENAME &
+	echo -e "${GREEN}Finished, bye${RESET}";
+	exit 0;
+}
+if [[ ! -d "Discord.app" ]]
+then
+	echo -e "${RED}Discord not found. Please make sure it is in the applications folder.${RESET}"
+elif [[ -d "Better Discord.app" ]] && [[ -d "./Better Discord.app/Contents/Resources/app/" ]]
+then
+	echo -e "${RED}You have already installed it!${RESET}"
+	echo -e "Would you like a ${VERYRED}complete${RESET} reinstall?"
+	select yn in "Yes" "No"; do
+		case $yn in
+			Yes ) installBD;;
+			No ) exit;;
+		esac
+	done
+elif [[ -d "Better Discord.app" ]] && [[ ! -d "./Better Discord.app/Contents/Resources/app/" ]]
+then
+	echo -e "${RED}Seems like it wasn't properlly installed. Reinstalling...${RESET}"
+	installBD
+elif [[ ! -d "Better Discord.app" ]] && [[ -d "Discord.app" ]] && [[ ! -d "./Better Discord.app/Contents/Resources/app" ]]
+then
+	installBD
 fi
